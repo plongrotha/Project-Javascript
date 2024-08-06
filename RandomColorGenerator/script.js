2c305a2976059ce84ab8d2d3bad26ffdc82b0e00
plong rotha <Mrr.rothabetta31@gmail.com>
2024-08-06 11:24:45 +0700

    add randomcolor method

--- RandomColorGenerator/script.js
+++ RandomColorGenerator/script.js
@@ -4,3 +4,14 @@ for (let index = 0; index < 30; index++) {
   colorContainer.classList.add("color-container");
   containerElement.appendChild(colorContainer);
 }
+randomcolor();
+function randomcolor() {
+  const chars = "0123456789abcdef";
+  const colorCodeLenght = 6;
+  let colorCode = "";
+  for (let index = 0; index < colorCodeLenght; index++) {
+    const randomNum = Math.floor(Math.random() * chars.length);
+    colorCode += chars.substring(randomNum, randomNum + 1);
+    console.log(colorCode);
+  }
+}
